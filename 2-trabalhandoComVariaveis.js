ffee69704adac299505976d03831f822ce9d66df
davidpaulati <32557397+davidpaulati@users.noreply.github.com>
2021-02-24 22:17:00 -0300

    Add files via upload

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "David";
+let ano = 2021;
+
+//não  fazer
+//mes = "Janeiro";
---
+++ 5-atribuicaoDeVariveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 29;
+let primeiroNome = "David";//variavel que pode variar
+const sobrenome = "Paula";
+
+//console.log(nome+ " " + sobrenome)
+console.log(primeiroNome,sobrenome); 
+
+//concatenando com crase
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
